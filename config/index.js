e02735b4fa52de36945b63a93f83d31401ea60ad
Arthur Vinot <74649978+ArthurVinot@users.noreply.github.com>
2022-01-17 23:30:56 +0000

    restart

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-if(process.env.NODE_ENV === 'production ') {
+if(process.env['NODE_ENV'] === 'production ') {
   module.exports = {
     FB: {
-      pageAccessToken: process.env.pageAccessToken ,
-      VerifyToken: process.env.VerifyToken
+      pageAccessToken: process.env['pageAccessToken'] ,
+      VerifyToken: process.env['VerifyToken']
     }
   }
 } else {
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
 const express = require('express');
 const config = require('./config');
-const server = express ();
+const server = express();
 const PORT = process.env.PORT || 3000;
-server.get('/', (req , res) => res.send("hello!"));
-server.listen(PORT , () => console.log('The bot server is running on port ${PORT}'));
+server.get('/', (req, res) => res.send("hello!"));
+server.listen(PORT, () => console.log('The bot server is running on port ${PORT}'));
 
 console.log(config.FB)
 
