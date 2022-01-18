7a689a4a9c24fcb3721dd4f12abf49ea0cfa6bdb
Arthur Vinot <74649978+ArthurVinot@users.noreply.github.com>
2022-01-18 00:28:44 +0000

    Update Replit

--- fbeamer/index.js
+++ fbeamer/index.js
@@ -15,27 +15,25 @@ class FBeamer{
             console.error(error);
         }
     }
-}
-
-function registerHook(req, res) 
-{
-    const params = req.query;
-    console.log(params);
-    const mode = params['hub.mode'],
-    token = params['hub.verify_token'],
-    challenge = params['hub.challenge'];
-    
-    try {
-        if (mode === 'subscribe' && token === 'verifytoken'){
-            console.log("The webhook is registered")
-            return res.send(challenge);
-        } else {
-            console.log("Could not register webhook");
-            return res.sendStatus(200);
+  registerHook(req, res) {
+  const params = req.query;
+  console.log(params);
+  const mode = params['hub.mode'],
+  token = params['hub.verify_token'],
+  challenge = params['hub.challenge'];
+  
+  try {
+      if (mode === 'subscribe' && token === VerifyToken){
+          console.log("The webhook is registered")
+          return res.send(challenge);
+      } else {
+          console.log("Could not register webhook");
+          return res.sendStatus(200);
         }
-    } catch(e) {
-        console.log(e);
+  } catch(e) {
+      console.log(e);
     }
+  }
 }
 
 module.exports = FBeamer;
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 'use strict';
 const express = require('express');
 const config = require('./config');
+const fbeamer = require('./fbeamer');
+const f = new fbeamer(config.FB);
 const server = express();
 const PORT = process.env.PORT || 3000;
-server.get('/', (req, res) => res.send("hello!"));
+server.get('/', (req, res) => f.registerHook(req, res));
 server.listen(PORT, () => console.log('The bot server is running on port ${PORT}'));
-
-console.log(config.FB)
-
-const FBeamer = require('./fbeamer');
-var f = new FBeamer(config.FB);
-console.log(f);
